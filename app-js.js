5de98e065b6a9de840eb60853c09d484415bc98f
William Cooper <william.cooper@nhs.net>
2019-01-09 20:19:28 +0000

    jokes

--- app-js.js
+++ app-js.js
@@ -1,1 +1,7 @@
-console.log('hello');
+function hover(element) {
+  element.setAttribute('src', 'images/the-jokes.jpg');
+}
+
+function unhover(element) {
+  element.setAttribute('src', 'images/the-jerks.jpg');
+}
