c5ab85ad2aed6d6a37fa9f0a5c5b89c288d4bd77
William Cooper <william.cooper@nhs.net>
2019-02-27 22:30:19 +0000

    index changes

--- app-js.js
+++ app-js.js
@@ -5,3 +5,31 @@ function hover(element) {
 function unhover(element) {
   element.setAttribute('src', 'images/the-jerks.jpg');
 }
+
+
+// Change nav bar on scroll
+ $(function(){
+  $(window).on('load scroll resize orientationchange', function () {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 10) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+          if($(this).scrollTop() > 10) { 
+              $('.nav-link').addClass('nav-scroll');
+          } else {
+              $('.nav-link').removeClass('nav-scroll');
+          }
+          if($(this).scrollTop() > 10) { 
+              $('.nav-item').addClass('scroll-text');
+          } else {
+              $('.nav-item').removeClass('scroll-text');
+          }
+          if($(this).scrollTop() > 10) { 
+              $('.nav-img').attr('src', 'images/mouse-green.png');
+          } else {
+              $('.nav-img').attr('src', 'images/mouse-white.png');
+          }
+        });
+});
