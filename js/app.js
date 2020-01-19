061967fa8ee5b3a53b1751ef31ddbc80f957246e
wicoo <willdcooper@gmail.com>
2020-01-19 18:16:03 +0000

    jquery button

--- js/app.js
+++ js/app.js
@@ -99,23 +99,17 @@ class LiteYTEmbed extends HTMLElement {
 // Register custome element
 customElements.define('lite-youtube', LiteYTEmbed);
 
-// TOP BUTTON
-//Get the button:
-mybutton = document.getElementById("back-top");
+// Show scroll to top
+$(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 2000) {
+      $('.top-button').fadeIn();
+    } else {
+      $('.top-button').fadeOut();
+    }
+  });
 
-// When the user scrolls down 1000px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  //on click scroll to top
+  $('#back-top').click(function(){
+    $('html, body').animate({scrollTop:0}, 'fast');
+  });
